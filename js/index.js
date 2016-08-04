6d4cf9e647ce01fb0a0bf236c077b687e15e542c
taolili <taolili@itcast.cn>
2016-08-04 19:01:19 +0800

    陶丽个人网站 1.0.0

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/**
+ * Created by li-hg on 2016/7/25.
+ */
+$(function(){
+    banner();
+});
+function banner(){
+    var data = [
+        {
+            "bac":"images/banner.jpg",
+            "img":"images/banner-m.jpg",
+        }
+    ]
+    var render = function(){
+        /*2.判断当前的屏幕  移动端   非移动端   小于768px*/
+        var width = $(window).width();
+        /*是否是移动端*/
+        var isMobile = false;
+        if(width < 768){
+            isMobile = true;
+        }
+
+        /*3.把我们的数据转化成html  underscore  template*/
+        /*点盒子*/
+
+        /*图片盒子*/
+
+        /*取到模版当中的字符串*/
+        var imageTemplateStr = $('#image_template').html();
+        /*转化成模版函数*/
+        var imageTemplate = _.template(imageTemplateStr);
+        /*传入数据 解析成 html 字符*/
+        var imageHtml = imageTemplate({model:data,isMobile:isMobile});//我们只需要再加一个属性
+        /*把html字符串渲染在页面当中*/
+        $('.carousel-inner').html(imageHtml);
+
+    }
+
+    /*5.监听页面尺寸改变  响应当前的尺寸   resize*/
+    $(window).on('resize',function(){
+        render();
+    }).trigger('resize');
+    /*我们想用js来触发一次resize  render（）就执行了一次*/
+
+}
---
+++ js/template.js
@@ -0,0 +1,52 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Author e-mail:htmlcoder.me@gmail.com
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Initializations of plugins 
+ */
+
+(function($){
+	$(document).ready(function(){
+		// Isotope filters
+		//-----------------------------------------------
+		if ($('.isotope-container').length>0) {
+			$(window).load(function() {
+				$('.isotope-container').fadeIn();
+				var $container = $('.isotope-container').isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: 'masonry',
+					transitionDuration: '0.6s',
+					filter: "*"
+				});
+				// filter items on button click
+				$('.filters').on( 'click', 'ul.nav li a', function() {
+					var filterValue = $(this).attr('data-filter');
+					$(".filters").find("li.active").removeClass("active");
+					$(this).parent().addClass("active");
+					$container.isotope({ filter: filterValue });
+					return false;
+				});
+			});
+		};
+
+		//Modal
+		//-----------------------------------------------
+		if($(".modal").length>0) {
+			$(".modal").each(function() {
+				$(".modal").prependTo( "body" );
+			});
+		}
+
+	}); // End document ready
+})(this.jQuery);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-40620479-8', 'auto');
+ga('send', 'pageview');
